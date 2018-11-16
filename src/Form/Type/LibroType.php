d844366c3459c53afbbad8f46cc7757206bfa074
Luis Mendoza <luis@atomoweb.com>
2018-11-16 16:07:42 -0400

    autor error

--- src/Form/Type/LibroType.php
+++ src/Form/Type/LibroType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use App\Form\Type\AutorType;
 
 class LibroType extends AbstractType
 {
