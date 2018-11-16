4c37c937f73455d9ba2094847efa5ed93313b183
Luis Mendoza <luis@atomoweb.com>
2018-11-16 16:05:03 -0400

    autor error

---
+++ src/Form/Type/AutorType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class AutorType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder->add('nombre', TextType::class, 
+				array('required'=>true,
+						'label' => 'Titulo'
+	));
+		//->add('save', 'submit', array('attr' => array('class'=>'buttonx')));
+	}
+
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(array(
+				'data_class' => 'App\Entity\Autor',
+		));
+	}
+
+	public function getBlockPrefix()
+	{
+		return 'autor';
+	}
+}
