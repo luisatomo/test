c12b9f23909cead4505bf6b2ab629c10ad961a42
Luis Mendoza <luis@atomoweb.com>
2018-11-16 16:26:22 -0400

    DateType missing

--- src/Form/Type/LibroType.php
+++ src/Form/Type/LibroType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use App\Form\Type\AutorType;
 
